123352fe9cb9477ccc72d41a45a8f12adff26745
Ahmet AYGÜN <me@ahmetaygun.net>
2011-12-09 03:23:38 +0200

    add initial setup.pu

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import os
+from setuptools import setup
+
+setup(
+        name='django-cart',
+        version='0.1.0',
+        description='Django session-based shopping cart.',
+        maintainer='Ahmet AYGÜN',
+        maintainer_email='me@ahmetaygun.net',
+        license="GNU v3",
+        url='https://github.com/ahmet/django-cart',
+        packages=['cart'],
+     )
