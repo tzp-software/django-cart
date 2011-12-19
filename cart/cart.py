60678d0ab0f40e86895e727c649fc175a5b9e04a
Ahmet AYGÜN <me@ahmetaygun.net>
2011-12-19 18:00:06 +0200

    add owner

--- cart/cart.py
+++ cart/cart.py
@@ -14,9 +14,10 @@ class ItemDoesNotExist(Exception):
 class Cart:
     def __init__(self, request):
         cart_id = request.session.get(CART_ID)
+        owner = request.user.id
         if cart_id:
             try:
-                cart = models.Cart.objects.get(id=cart_id, checked_out=False)
+                cart = models.Cart.objects.get(id=cart_id, owner_id=owner, checked_out=False)
             except models.Cart.DoesNotExist:
                 cart = self.new(request)
         else:
@@ -28,7 +29,7 @@ class Cart:
             yield item
 
     def new(self, request):
-        cart = models.Cart(creation_date=datetime.datetime.now())
+        cart = models.Cart(owner=request.user.id, creation_date=datetime.datetime.now())
         cart.save()
         request.session[CART_ID] = cart.id
         return cart
